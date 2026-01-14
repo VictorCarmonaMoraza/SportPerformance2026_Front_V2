3dc24fcf0aba3b8028ff6be5a27f1e4d56825efe
Victor Carmona <VictorCarmona9035@hotmail.com>
2026-01-14 21:49:06 +0100

    Refactor codigo

--- src/app/sportPerformance/pages/user-metrics-page/user-metrics-page.ts
+++ src/app/sportPerformance/pages/user-metrics-page/user-metrics-page.ts
@@ -1,17 +1,16 @@
-import { DatePipe } from '@angular/common';
 import { Component, computed, effect, inject, signal } from '@angular/core';
+import ChartPrediction from '../../../shared/components/chart-prediction/chart-prediction';
 import { EvolutionByWeek } from '../../../shared/components/evolution-by-week/evolution-by-week';
 import { EvolutionByYear } from '../../../shared/components/evolution-by-year/evolution-by-year';
+import { PredictionApi } from '../../../shared/interfaces/prediction-calories-interface';
+import { MachineLearningService } from '../../../shared/services/machine-learning-service';
 import { MetricsService } from '../../../shared/services/metrics-service';
 import { SportService } from '../../../shared/services/sport-service';
-import { MachineLearningService } from '../../../shared/services/machine-learning-service';
-import { PredictionApi } from '../../../shared/interfaces/prediction-calories-interface';
-import ChartPrediction from '../../../shared/components/chart-prediction/chart-prediction';
 
 
 @Component({
   selector: 'app-user-metrics-page',
-  imports: [EvolutionByWeek, EvolutionByYear, DatePipe, ChartPrediction],
+  imports: [EvolutionByWeek, EvolutionByYear, ChartPrediction],
   templateUrl: './user-metrics-page.html',
   styleUrl: './user-metrics-page.css',
 })
@@ -23,28 +22,9 @@ export class UserMetricsPage {
   title: string = '';
   unidad = 'cal'
 
-  /* ===== Signals compartidos ===== */
-  ultimasCalorias = this.metricsService.ultimasCalorias;
-  ultimoPeso = this.metricsService.ultimoPeso;
-  ultimaFecha = this.metricsService.ultimaFecha;
-
-  /* ===== Fecha ACTUAL FIJA (NO reactiva) ===== */
-  private readonly ahora = new Date();
-  // fecha actual FIJA (no reactiva)
-  readonly fechaActual = signal(new Date());
-
   /* ===== Recursos ===== */
   readonly infoUserResource = this.sportService.infoUserResource;
   readonly deportista = this.sportService.deportista;
-  readonly lastMetricResource = this.metricsService.lastMetricResource;
-
-  readonly lastMetric = computed(() => {
-    const response = this.lastMetricResource.value();
-    return response?.metrics?.length ? response.metrics[0] : null;
-  });
-
-
-
 
   constructor() {
     effect(() => {
@@ -55,35 +35,7 @@ export class UserMetricsPage {
 
       this.title = user.deportista.disciplina_deportiva;
     });
-
-
-    // ⬇️ NUEVO EFFECT: última métrica
-    effect(() => {
-      const metric = this.lastMetric();
-      if (!metric) return;
-
-      this.metricsService.setUltimaFecha(metric.fecha);
-
-      if (metric.calorias != null) {
-        this.metricsService.setUltimasCalorias(Number(metric.calorias));
-      }
-
-      if (metric.peso != null) {
-        this.metricsService.setUltimoPeso(Number(metric.peso));
-      }
-    });
   }
-
-  /* ===== DÍAS DESDE ÚLTIMA FECHA (ESTABLE) ===== */
-  readonly diasSinEntrenar = computed<number | null>(() => {
-    const fecha = this.ultimaFecha();
-    if (!fecha) return null;
-
-    const fechaUltima = new Date(fecha);
-    const diffMs = this.ahora.getTime() - fechaUltima.getTime();
-
-    return Math.floor(diffMs / (1000 * 60 * 60 * 24));
-  });
 
   PredictionCalories() {
     //Llamamos al metodo predicciones con el id del usuario
